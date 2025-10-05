f1094fdba998f7b234b0d1ff3a728845468710db
Allan Vidal <alnvdl@users.noreply.github.com>
2025-10-05 13:23:49 +0000

    Add service worker to make Diagrama available offline

---
+++ src/sw.js
@@ -0,0 +1,54 @@
+const CACHE_NAME = 'diagrama-cache-v1';
+
+// Remember to add new URLs in case you introduce new files in src.
+const OFFLINE_URLS = [
+    '/',
+    'index.html',
+    'diagrama.js',
+    'diagrama.css',
+    'manifest.json',
+    'icon16.png',
+    'icon48.png',
+    'icon180.png',
+    'icon.svg',
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(OFFLINE_URLS)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+    if (event.request.method !== 'GET') return;
+    event.respondWith(
+        // Try the network first.
+        fetch(event.request)
+            .then(response => {
+                // Update the cache if there's a hit.
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then(cache => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => {
+                // Cannot fetch, fallback to cache.
+                return caches.match(event.request).then(cached => {
+                    if (cached) return cached;
+                    // Fallback to / for navigation requests.
+                    if (event.request.mode === 'navigate') {
+                        return caches.match('/');
+                    }
+                });
+            })
+    );
+});
